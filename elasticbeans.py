f7d10520d74da4a6e6e8c19751d74a52f803dd2c
mnswifi <yusufabdulganiyu@outlook.com>
2024-05-21 06:53:08 +0100

    edited elasticbeans config file

--- elasticbeans.py
+++ elasticbeans.py
@@ -2,22 +2,34 @@ import boto3
 
 eb_client = boto3.resource('elasticbeanstalk')
 
-eb_client.create_application(
-    ApplicationName='DevOpsGettingStarted',
-    Description='Hosting web app to test CI/CD using github'
-)
+def create_application():
+    eb_client.create_application(
+        ApplicationName='DevOpsGettingStarted',
+        Description='Hosting web app to test CI/CD using github'
+    )
+def create_environment():
+    eb_client.create_environment(
+        ApplicationName = "DevOpsGettingStarted",
+        EnvironmentName = "DevOpsGettingStarted-env",
+        Description="elastic beanstalk environment for CI delivery using github",
+        Tier={
+            'Name':'WebServer',
+            'Type': 'Standard'
+        }
+    )
 
-eb_client.create_environment(
-    ApplicationName = "DevOpsGettingStarted",
-    EnvironmentName = "DevOpsGettingStarted-env",
-    Description="elastic beanstalk environment for CI delivery using github",
-    Tier={
-        'Name':'WebServer',
-        'Type': 'Standard'
-    }
-)
-eb_client.create_platform_version(
-    PlatformName='Node.js',
-    PlatformVersion="6.0.0 (Recommended)",
-    EnvironmentName = "DevOpsGettingStarted-env"
-)
+def create_platform():
+    eb_client.create_platform_version(
+        PlatformName='Node.js',
+        PlatformVersion="6.0.0 (Recommended)",
+        EnvironmentName = "DevOpsGettingStarted-env"
+    )
+
+def main():
+    create_application()
+    create_application()
+    create_platform()
+
+
+if __name__=="__main__":
+    main()
